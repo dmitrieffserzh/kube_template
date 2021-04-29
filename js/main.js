539489f6fbe3856227b20080c2aa487d79997de0
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2021-04-29 14:34:04 +0300

    upadete

--- js/main.js
+++ js/main.js
@@ -3,5 +3,22 @@ $K.dom(".menu-button").on("click", function () {
         $K.dom("body").addClass("menu-open");
     } else {
         $K.dom("body").removeClass("menu-open");
-    };
+    }
+    ;
 });
+
+// MODAL WINDOW
+(function($K) {
+    $K.add('module', 'application', {
+        init: function(app) {
+            this.app = app;
+            this.modal = app.modal;
+        },
+        // events
+        onclick: function(e, element, type) {
+            if (type === 'showCallback') {
+                this.modal.open({ title: 'Заказать звонок', target: '#callback', width: '400px' });
+            }
+        }
+    });
+})(Kube);
